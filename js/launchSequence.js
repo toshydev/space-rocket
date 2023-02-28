60a9b674f55a65f09fe30afd4316ed91b178586e
Anton Roters <antonroters@hotmail.com>
2023-02-28 13:17:22 +0100

    complete launch function

--- js/launchSequence.js
+++ js/launchSequence.js
@@ -1,1 +1,20 @@
+import { loadPayload } from "./core/load.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+
 // Implement the launch sequence function here and export it as the default export.
+export default function launch() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  liftoff();
+  deployPayload();
+}
